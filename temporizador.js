462197cc164619b427f7404a7971e16926073928
JJBriceL <82071656+JJBriceL@users.noreply.github.com>
2021-07-17 21:24:50 -0500

    tempo

--- temporizador.js
+++ temporizador.js
@@ -1,29 +1,29 @@
 
-// Set the date we're counting down to
-var countDownDate = new Date("Jul 17, 2021 22:40:00").getTime();
+// La fecha y hora a la que estamos contando
+var countDownDate = new Date("Jul 17, 2021 22:55:00").getTime();
 
-// Update the count down every 1 second
+// Actualiza el conteo cada segundo
 var x = setInterval(function() {
 
-  // Get today's date and time
+  // Obtiene la fecha y hora actual (de tu compu)
   var now = new Date().getTime();
     
-  // Find the distance between now and the count down date
+  // Calcula la diferencia entre la hora actual y la hora a contar
   var distance = countDownDate - now;
     
-  // Time calculations for days, hours, minutes and seconds
+  // Calculo por dias, horas, minutos y segundos
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
-  // Output the result in an element with id="demo"
+  // Muestra el resultado con "demo"
   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
     
-  // If the count down is over, write some text 
+  // Escribe un texto  cuando el contador haya acabado
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("demo").innerHTML = "Acabó el ciclo ;_;";
   }
 }, 1000);
